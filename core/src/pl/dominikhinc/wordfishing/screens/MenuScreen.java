cbc4208251fa947d0d06b26487d99a61d2bd57ac
Kseniu <46762501+Kseniu@users.noreply.github.com>
2019-01-17 16:28:52 +0100

    Added Background to Menu

--- core/src/pl/dominikhinc/wordfishing/screens/MenuScreen.java
+++ core/src/pl/dominikhinc/wordfishing/screens/MenuScreen.java
@@ -1,9 +1,17 @@
 package pl.dominikhinc.wordfishing.screens;
 
 
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
 import pl.dominikhinc.wordfishing.WordFishing;
 
 public class MenuScreen extends AbstractScreen {
+
+    private Image bgImg;
+    private Button grajButton;
 
     public MenuScreen(WordFishing game){
         super(game);
@@ -11,7 +19,31 @@ public class MenuScreen extends AbstractScreen {
 
     @Override
     protected void init(){
+        initBg();
+        initGrajButton();
+    }
 
+    private void initGrajButton() {
+        grajButton = new Button()
 
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+    }
+
+    private void initBg() {
+        //TODO add bettr bgImage
+        bgImg = new Image(new Texture("Temporary.Menu.Background.png"));
+        stage.addActor(bgImg);
     }
 }
