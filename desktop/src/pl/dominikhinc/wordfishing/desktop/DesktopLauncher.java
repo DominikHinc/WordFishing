1225cff83ae2e0fa9bf7866a870a984c258d6c3a
Kseniu <46762501+Kseniu@users.noreply.github.com>
2019-01-17 15:32:28 +0100

    First configurations lines added

--- desktop/src/pl/dominikhinc/wordfishing/desktop/DesktopLauncher.java
+++ desktop/src/pl/dominikhinc/wordfishing/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import pl.dominikhinc.wordfishing.WordFishing;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = WordFishing.SCREEN_WIDTH;
+		config.height = WordFishing.SCREEN_HEIGHT;
+		config.title = WordFishing.GAME_TITLE;
+		//config.resizable = false;
+
 		new LwjglApplication(new WordFishing(), config);
 	}
 }
